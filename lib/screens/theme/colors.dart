8ca3e6fa968cafd97c5c4f1e14845209225b3ce5
nayeemtby <nayeemtby@gmail.com>
2021-12-26 07:35:19 +0600

    Created theme components

---
+++ lib/screens/theme/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color bubbleColor = Color(0x99E1E1E1);
+  static const Color primaryBlack = Color(0xf0000000);
+  static const Color deepBlack = Color(0xff000000);
+  static const Color secondaryBlack = Color(0x80000000);
+  static const Color primaryWhite = Color(0xffffffff);
+  static const Color secondaryWhite = Color(0xd9ffffff);
+  static const Color primaryGray = Color(0xff929292);
+  static const Color silver = Color(0x85C4C4C4);
+  static const Color primaryBlue = Color(0xff007AFF);
+  static const Color primaryRed = Color(0x85EA4242);
+}
---
+++ lib/screens/theme/texttheme.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class TxtTheme {
+  static TextStyle montyBig = TextStyle(
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+    fontSize: 53.sp,
+  );
+  static TextStyle montySmall = TextStyle(
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w500,
+    fontSize: 19.sp,
+  );
+
+  static TextStyle reg20 = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 20.sp,
+  );
+  static TextStyle reg17 = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 17.sp,
+  );
+  static TextStyle reg13 = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 13.sp,
+  );
+  static TextStyle reg15 = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 15.sp,
+  );
+  static TextStyle reg19 = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 19.sp,
+  );
+  static TextStyle reg16 = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 16.sp,
+  );
+  static TextStyle med17 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 17.sp,
+  );
+  static TextStyle med32 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 32.sp,
+  );
+  static TextStyle med16 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 16.sp,
+  );
+  static TextStyle med14 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 14.sp,
+  );
+  static TextStyle med18 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 18.sp,
+  );
+  static TextStyle med13 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 13.sp,
+  );
+}
