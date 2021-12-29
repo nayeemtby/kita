db69d84add606634cc8fd31cfd43894e4d5550ed
nayeemtby <nayeemtby@gmail.com>
2021-12-29 17:58:52 +0600

    Added login function

--- lib/screens/components/firebase.dart
+++ lib/screens/components/firebase.dart
@@ -14,3 +14,19 @@ Future<dynamic> signUp(String email, String password) async {
   }
   return response;
 }
+
+Future<dynamic> login(String email, String password) async {
+  Client client = Client();
+  final FirebaseAuth fbAuth = FirebaseAuth(client, apiKey);
+  dynamic response;
+  try {
+    response = fbAuth.signInWithPassword(
+      email,
+      password,
+    );
+  } catch (e) {
+    response = e;
+    print(e);
+  }
+  return response;
+}
