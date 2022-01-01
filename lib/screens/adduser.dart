1b2e579cfc3f7cf50e411d339dfc7f95adb5227e
nayeemtby <nayeemtby@gmail.com>
2022-01-01 11:58:14 +0600

    Fixed addUser page

--- lib/screens/adduser.dart
+++ lib/screens/adduser.dart
@@ -21,175 +21,190 @@ class _AddUserPageState extends State<AddUserPage> {
   int _gender = 0;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.w),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(
-              vertical: 14.h,
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.w),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: 10.h,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  CupertinoButton(
+                    padding: const EdgeInsets.all(0),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(
+                          Icons.close,
+                          size: 18.sp,
+                          color: MyColors.primaryBlue,
+                        ),
+                      ],
+                    ),
+                    onPressed: () {
+                      widget.parentSetState(() {});
+                    },
+                  ),
+                  Text(
+                    'Add New User',
+                    style: TxtTheme.med18.copyWith(
+                      color: MyColors.deepBlack,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 24.sp,
+                  ),
+                ],
+              ),
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                CupertinoButton(
-                  padding: const EdgeInsets.all(0),
-                  child: Row(
+            Expanded(
+                child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Stack(
+                      clipBehavior: Clip.none,
+                      children: [
+                        CircleAvatar(
+                          radius: 56.r,
+                        ),
+                        Positioned(
+                          bottom: -10.r,
+                          left: 5.r,
+                          child: CircleAvatar(
+                            radius: 20.r,
+                            backgroundColor: MyColors.deepBlack,
+                            child: Icon(
+                              Icons.camera_alt,
+                              size: 20.r,
+                              color: MyColors.primaryWhite,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40.h,
+                  ),
+                  TxtInput(
+                    hint: 'Name',
+                  ),
+                  SizedBox(
+                    height: 12.h,
+                  ),
+                  TxtInput(hint: 'Email'),
+                  SizedBox(
+                    height: 12.h,
+                  ),
+                  TxtInput(
+                    hint: 'Password',
+                    password: true,
+                    suffix: Icon(
+                      Icons.visibility,
+                      color: MyColors.secondaryBlack,
+                      size: 24.sp,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12.h,
+                  ),
+                  TxtInput(
+                    hint: 'Mobile Number',
+                    password: true,
+                    suffix: Text(
+                      'Verify',
+                      style: TxtTheme.reg15.copyWith(
+                        color: MyColors.primaryBlue,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 14.h,
+                  ),
+                  Text(
+                    'Gender',
+                    style: TxtTheme.reg19.copyWith(
+                      color: MyColors.primaryBlack,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16.h,
+                  ),
+                  Row(
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                      Icon(
-                        Icons.close,
-                        size: 17.sp,
-                        color: MyColors.primaryBlue,
+                      Text(
+                        'Male',
+                        style: TxtTheme.reg19.copyWith(
+                          color: MyColors.deepBlack,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 16.w,
+                      ),
+                      Radio<int>(
+                        fillColor:
+                            MaterialStateProperty.all(MyColors.deepBlack),
+                        value: 0,
+                        groupValue: _gender,
+                        onChanged: (Object? value) {
+                          setState(() {
+                            _gender = value as int;
+                          });
+                        },
+                      ),
+                      SizedBox(
+                        width: 42.w,
+                      ),
+                      Text(
+                        'Female',
+                        style: TxtTheme.reg19.copyWith(
+                          color: MyColors.deepBlack,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 16.w,
+                      ),
+                      Radio<int>(
+                        fillColor:
+                            MaterialStateProperty.all(MyColors.deepBlack),
+                        value: 1,
+                        groupValue: _gender,
+                        onChanged: (Object? value) {
+                          setState(() {
+                            _gender = value as int;
+                          });
+                        },
                       ),
                     ],
                   ),
-                  onPressed: () {
-                    widget.parentSetState(() {});
-                  },
-                ),
-                Text(
-                  'Add New User',
-                  style: TxtTheme.med18.copyWith(
-                    color: MyColors.deepBlack,
+                  SizedBox(
+                    height: 32.h,
                   ),
-                ),
-                SizedBox(
-                  width: 24.sp,
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 36.h,
-          ),
-          Center(
-            child: Stack(
-              clipBehavior: Clip.none,
-              children: [
-                CircleAvatar(
-                  radius: 56.r,
-                ),
-                Positioned(
-                  bottom: -10.r,
-                  left: 5.r,
-                  child: CircleAvatar(
-                    radius: 20.r,
-                    backgroundColor: MyColors.deepBlack,
-                    child: Icon(
-                      Icons.camera_alt,
-                      size: 20.r,
-                      color: MyColors.primaryWhite,
-                    ),
+                  PrimaryBtn(
+                    txt: 'Save',
+                    onTap: () {},
                   ),
-                )
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 60.h,
-          ),
-          TxtInput(
-            hint: 'Name',
-          ),
-          SizedBox(
-            height: 12.h,
-          ),
-          TxtInput(hint: 'Email'),
-          SizedBox(
-            height: 12.h,
-          ),
-          TxtInput(
-            hint: 'Password',
-            password: true,
-            suffix: Icon(
-              Icons.visibility,
-              color: MyColors.secondaryBlack,
-              size: 24.sp,
-            ),
-          ),
-          SizedBox(
-            height: 12.h,
-          ),
-          TxtInput(
-            hint: 'Mobile Number',
-            password: true,
-            suffix: CupertinoButton(
-              child: Text(
-                'Verify',
-                style: TxtTheme.reg15,
+                ],
               ),
-              onPressed: () {},
-              padding: const EdgeInsets.all(0),
-            ),
-          ),
-          SizedBox(
-            height: 14.h,
-          ),
-          Text(
-            'Gender',
-            style: TxtTheme.reg19.copyWith(
-              color: MyColors.primaryBlack,
-            ),
-          ),
-          SizedBox(
-            height: 16.h,
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                'Male',
-                style: TxtTheme.reg19.copyWith(
-                  color: MyColors.deepBlack,
-                ),
-              ),
-              SizedBox(
-                width: 16.w,
-              ),
-              Radio<int>(
-                fillColor: MaterialStateProperty.all(MyColors.deepBlack),
-                value: 0,
-                groupValue: _gender,
-                onChanged: (Object? value) {
-                  setState(() {
-                    _gender = value as int;
-                  });
-                },
-              ),
-              SizedBox(
-                width: 42.w,
-              ),
-              Text(
-                'Female',
-                style: TxtTheme.reg19.copyWith(
-                  color: MyColors.deepBlack,
-                ),
-              ),
-              SizedBox(
-                width: 16.w,
-              ),
-              Radio<int>(
-                fillColor: MaterialStateProperty.all(MyColors.deepBlack),
-                value: 1,
-                groupValue: _gender,
-                onChanged: (Object? value) {
-                  setState(() {
-                    _gender = value as int;
-                  });
-                },
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 32.h,
-          ),
-          PrimaryBtn(
-            txt: 'Save',
-            onTap: () {},
-          ),
-        ],
+            ))
+          ],
+        ),
       ),
     );
   }
