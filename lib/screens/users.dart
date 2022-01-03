5d5ec4da1b48fbc35773b8b62a355e37769b8dfc
nayeemtby <nayeemtby@gmail.com>
2022-01-03 19:42:27 +0600

    Fix bugs

--- lib/screens/users.dart
+++ lib/screens/users.dart
@@ -34,149 +34,158 @@ class _UsersPageState extends State<UsersPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: EdgeInsets.symmetric(
-            vertical: 14.h,
-          ),
-          child: Center(
-            child: Text(
-              'User List',
-              style: TxtTheme.med18.copyWith(
-                color: MyColors.deepBlack,
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(
+              vertical: 14.h,
+            ),
+            child: Center(
+              child: Text(
+                'User List',
+                style: TxtTheme.med18.copyWith(
+                  color: MyColors.deepBlack,
+                ),
               ),
             ),
           ),
-        ),
-        Divider(
-          thickness: 1.sp,
-          height: 1.sp,
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 24.h),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 24.w),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Icon(
-                      Icons.person_outline_rounded,
-                      size: 24.sp,
-                    ),
-                    Text(
-                      'Total Users\n${isLoading ? '--' : snap.docs.length - 1}',
-                      textAlign: TextAlign.center,
-                      style: TxtTheme.med14.copyWith(
-                        color: MyColors.deepBlack,
+          Divider(
+            thickness: 1.sp,
+            height: 1.sp,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 24.h),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 24.w),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        Icons.person_outline_rounded,
+                        size: 24.sp,
                       ),
-                    )
-                  ],
+                      Text(
+                        'Total Users\n${isLoading ? '--' : snap.docs.length - 1}',
+                        textAlign: TextAlign.center,
+                        style: TxtTheme.med14.copyWith(
+                          color: MyColors.deepBlack,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              Material(
-                borderRadius: BorderRadius.circular(7.r),
-                color: MyColors.deepBlack,
-                child: InkWell(
-                  onTap: () {
-                    widget.showAddPage();
-                  },
+                Material(
                   borderRadius: BorderRadius.circular(7.r),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 14.w,
-                      vertical: 6.h,
-                    ),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(
-                          Icons.person_add_alt,
-                          size: 14.sp,
-                          color: MyColors.primaryWhite,
-                        ),
-                        SizedBox(
-                          width: 6.w,
-                        ),
-                        Text(
-                          'Add New User',
-                          style: TxtTheme.reg13.copyWith(
+                  color: MyColors.deepBlack,
+                  child: InkWell(
+                    onTap: () {
+                      widget.showAddPage();
+                    },
+                    borderRadius: BorderRadius.circular(7.r),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 14.w,
+                        vertical: 6.h,
+                      ),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(
+                            Icons.person_add_alt,
+                            size: 14.sp,
                             color: MyColors.primaryWhite,
                           ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16.w),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TxtInput(
-                hint: 'Search',
-                leading: Padding(
-                  padding: EdgeInsets.only(left: 4.w),
-                  child: Icon(
-                    Icons.search,
-                    size: 24.h,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 14.h,
-              ),
-              Divider(
-                thickness: 1.h,
-                height: 1.h,
-              ),
-            ],
-          ),
-        ),
-        Expanded(
-          child: isLoading
-              ? Center(
-                  child: CupertinoActivityIndicator(
-                    radius: 10.r,
-                  ),
-                )
-              : SingleChildScrollView(
-                  physics: const BouncingScrollPhysics(),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16.w),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: List.generate(
-                        snap.docs.length,
-                        (index) {
-                          if (snap.docs[index].id == currentUser) {
-                            return const SizedBox();
-                          }
-                          Map<String, dynamic> data =
-                              snap.docs[index].data() as Map<String, dynamic>;
-                          return UserItem(
-                            remove: removeUser,
-                            id: snap.docs[index].id,
-                            data: UserData(
-                              email: data['email'],
-                              name: data['name'],
-                              imgurl: data['imgurl'],
+                          SizedBox(
+                            width: 6.w,
+                          ),
+                          Text(
+                            'Add New User',
+                            style: TxtTheme.reg13.copyWith(
+                              color: MyColors.primaryWhite,
                             ),
-                          );
-                        },
+                          )
+                        ],
                       ),
                     ),
                   ),
                 ),
-        )
-      ],
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.w),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TxtInput(
+                  hint: 'Search',
+                  leading: Padding(
+                    padding: EdgeInsets.only(left: 4.w),
+                    child: Icon(
+                      Icons.search,
+                      size: 24.h,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 14.h,
+                ),
+                Divider(
+                  thickness: 1.h,
+                  height: 1.h,
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: isLoading
+                ? Center(
+                    child: CupertinoActivityIndicator(
+                      radius: 10.r,
+                    ),
+                  )
+                : SingleChildScrollView(
+                    physics: const BouncingScrollPhysics(),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16.w),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: List.generate(
+                          snap.docs.length,
+                          (index) {
+                            if (snap.docs[index].id == currentUser) {
+                              return const SizedBox();
+                            }
+                            Map<String, dynamic> data =
+                                snap.docs[index].data() as Map<String, dynamic>;
+                            return UserItem(
+                              remove: removeUser,
+                              id: snap.docs[index].id,
+                              data: UserData(
+                                email: data['email'],
+                                name: data['name'],
+                                imgurl: data['imgurl'],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+          )
+        ],
+      ),
     );
   }
 
